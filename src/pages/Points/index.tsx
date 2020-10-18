194a236574dbe4900964bdcccc2accef372706e6
Maick Souza <maick_a_s@msn.com>
2020-10-18 13:28:30 -0300

    feat(mobile): Navigation #3

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
+import { View, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import styles from '../Home/styles';
 
 const Points: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  const handleNavigateBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handleNavigateBack}>
+        <Icon name="arrow-left" size={20} color="#34cb79" />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default Points;
