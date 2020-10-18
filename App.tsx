87bfc7ead0b586327c2ca7d49f08dc805aea17fa
Maick Souza <maick_a_s@msn.com>
2020-10-18 13:05:32 -0300

    feat(mobile): Navigation #1

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 const App: React.FC = () => {
   const [fontsLoaded] = useFonts({
@@ -25,7 +25,7 @@ const App: React.FC = () => {
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      <Routes />
     </>
   );
 };
