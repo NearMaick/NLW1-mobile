06da111fcb24bd21b60ecb425fc7a99801fd8722
Maick Souza <maick_a_s@msn.com>
2020-10-18 12:33:01 -0300

    feat(mobile): Home page #2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, ImageBackground, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
@@ -16,6 +17,15 @@ const Home: React.FC = () => {
         <Text style={styles.description}>
           Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
         </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Text>{'>'}</Text>
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
       </View>
     </ImageBackground>
   );
