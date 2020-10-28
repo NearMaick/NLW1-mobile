5858e3ffce29e22161ea2ca6b1b8343985974b08
Maick Souza <maick_a_s@msn.com>
2020-10-28 05:17:56 -0300

    feat(mobile): Points Page #3

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,8 +1,59 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import {
+  TouchableOpacity,
+  View,
+  Image,
+  Text,
+  SafeAreaView,
+} from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import styles from './styles';
 
 const Detail: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  const handleNavigateBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Image
+          style={styles.pointImage}
+          source={{
+            uri:
+              'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+          }}
+        />
+        <Text style={styles.pointName}>Mercado do Jorge</Text>
+        <Text style={styles.pointItems}>Lâmpadas, óleo e cozinha</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Belo Jardim, PE</Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={20} color="#fff" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name="mail" size={20} color="#fff" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Detail;
